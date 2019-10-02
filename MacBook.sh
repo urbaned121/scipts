27d0ab01abdd8574ca0be3058acc90302c97ff2e
urbaned121 <31166488+urbaned121@users.noreply.github.com>
2019-10-02 16:20:50 +0200

    Add files via upload

---
+++ MacBook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Skrypt automatyzujacy instalacje brakujacych paczek w OSx
+
+#instalacja HomeBrew oraz cask
+#sudo yes "" |  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
+#sleep 3
+DOCTOR=$(brew doctor)
+echo $DOCTOR
+echo "++++++ Instalacja cask ++++++"
+sleep 1
+brew install cask
+
+#narzedzia
+brew install wget
+brew install htop
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install slack
+brew install git
+brew cask install skype
