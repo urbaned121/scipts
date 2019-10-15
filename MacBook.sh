3710ac677a6b3733bb2cf75946ca3d35a99e3e59
urbaned121 <dawurb@gmail.com>
2019-10-15 21:30:20 +0200

    Fixed home directory creation, added chceck for UniqueID

--- MacBook.sh
+++ MacBook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Skrypt automatyzujacy instalacje brakujacych paczek w OSx 
 #oraz tworzący konto uzytkownika z hasłem "Test123!" oraz uprawnieniami admina
-if [[ "$1" == '' ]] || [[ "$2" == '' ]]
+if [[ "$1" == '' ]] || [[ "$2" == '' ]];
 then
     echo "Podaj jako "'$1'" Imie i Nazwisko, jako "'$2'"  inazwisko w celu utworzenia konta uzytkownika"
     exit 0
@@ -12,29 +12,37 @@ sudo yes "" |  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/
 sleep 2
 DOCTOR=$(brew doctor)
 echo $DOCTOR
-echo -e "\e[42m++++++ Instalacja cask ++++++"
 sleep 1
+echo -e "++++++ Instalacja cask ++++++"
 brew install cask
 sleep 1
 #narzedzia
-brew install wget
-brew install htop
-brew cask install google-chrome
+brew install wget htop git
+brew cask install google-chrome 
 brew cask install visual-studio-code
 brew cask install slack
-brew install git
 brew cask install skype
 
-sudo dscl . -create /Users/$2
-sudo dscl . -create /Users/$2 UserShell /bin/bash
-sudo dscl . -create /Users/$2 RealName "$1"
-sudo dscl . -create /Users/$2 UniqueID 551
-sudo dscl . -create /Users/$2 PrimaryGroupID 80
-sudo dscl . -create /Users/$2 NFSHomeDirectory /Users/$2
-sudo dscl . -passwd /Users/$2 Test123!
-sudo dscl . -append /Groups/admin GroupMembership $2
-sudo scutil --set HostName espeo-$2
-sudo scutil --set ComputerName espeo-$2
-sudo scutil --set LocalHostName espeo-$2 
+if [[ $2 == "dscl . -list /Users UniqueID | awk '{print $1}' | grep -w $2" ]]; 
+then
+    echo "User already exists!"
+    exit 0
+fi
+
+LastID=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -n | tail -1)
+NextID=$((LastID + 1))
+
+sudo dscl . -create /Users/"$2"
+sudo dscl . -create /Users/"$2" UserShell /bin/bash
+sudo dscl . -create /Users/"$2" RealName "$1"
+sudo dscl . -create /Users/"$2" UniqueID $NextID
+sudo dscl . -create /Users/"$2" PrimaryGroupID 20
+sudo dscl . -create /Users/"$2" NFSHomeDirectory /Users/"$2"
+sudo dscl . -passwd /Users/"$2" Test123!
+sudo dscl . -append /Groups/admin GroupMembership "$2"
+sudo scutil --set HostName espeo-"$2"
+sudo scutil --set ComputerName espeo-"$2"
+sudo scutil --set LocalHostName espeo-"$2" 
+sudo createhomedir -u "$2" -c
 sudo fdesetup enable #turns on FileVault
 ioreg -l | grep IOPlatformSerialNumber
