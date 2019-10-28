33192552bf2cdbb13cd8a3b19379bd0617d40c5b
urbaned121 <dawurb@gmail.com>
2019-10-28 16:15:13 +0100

    some filevault fixes

--- MacBook.sh
+++ MacBook.sh
@@ -10,7 +10,7 @@ then
 fi  
 RealName=${1}
 UserName=${2}
-#instalacja HomeBrew oraz cask
+#installing homebrew & etc.
 sudo yes "" |  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 sleep 2
 brew doctor
@@ -19,20 +19,20 @@ sleep 1
 echo -e "++++++ Instalacja cask ++++++"
 brew install cask
 sleep 1
-#narzedzia
+#common used tools
 brew install wget htop git
 brew cask install google-chrome 
 brew cask install visual-studio-code
 brew cask install slack
 brew cask install skype
 brew cask install docker
-
+#chcecking if user with $Username already exist 
 if  [[ $UserName == $(dscl . -list /Users UniqueID | awk '{print $1}' | grep -w $UserName) ]]; 
 then
     echo "User already exists!"
     exit 0
 fi
-
+#increment UniqeID if we creating more than one new user 
 LastID=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -n | tail -1)
 NextID=$((LastID + 1))
 
@@ -51,5 +51,7 @@ sudo createhomedir -u "$UserName" -c
 ioreg -l | grep IOPlatformSerialNumber
 echo "New user $(dscl . -list /Users UniqueID | awk '{print $1}' | grep -w $UserName) has been created with unique ID $(dscl . -list /Users UniqueID | grep -w $UserName | awk '{print $2}')"
 
-sudo fdesetup enable #turns on FileVault
+#enabling FileVault and add $UserName to list of  users available to unlock encrypted disk
+sudo fdesetup enable 
 sudo fdesetup add -usertoadd "$UserName"
+printf "Users avaiable to unlock disk: %s\n$(sudo fdesetup list)'" | cut -d',' -f1 
