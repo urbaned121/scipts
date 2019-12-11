b58fe95b72625e6576d94c5c57ee5f3eca06b024
urbaned121 <dawurb@gmail.com>
2019-12-11 13:11:40 +0100

    experiment with fdesetup command

--- MacBook.sh
+++ MacBook.sh
@@ -35,9 +35,11 @@ fi
 #increment UniqeID if we creating more than one new user 
 LastID=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -n | tail -1)
 NextID=$((LastID + 1))
+CurrentUser=$USER
+echo "Creating user directory, enter admin password:"
 
 sudo dscl . -create /Users/"$UserName"
-sudo dscl . -create /Users/"$UserName" UserShell /bin/bash
+sudo dscl . -create /Users/"$UserName" UserShell /bin/zsh
 sudo dscl . -create /Users/"$UserName" RealName "$RealName"
 sudo dscl . -create /Users/"$UserName" UniqueID $NextID
 sudo dscl . -create /Users/"$UserName" PrimaryGroupID 80
@@ -47,13 +49,15 @@ sudo dscl . -append /Groups/admin GroupMembership "$UserName"
 sudo scutil --set HostName espeo-"$UserName"
 sudo scutil --set ComputerName espeo-"$UserName"
 sudo scutil --set LocalHostName espeo-"$UserName" 
-echo "Creating user directory, enter admin password:"
+printf "Creating new user directory, enter admin password:"
 sudo createhomedir -u "$UserName" -c
 ioreg -l | grep IOPlatformSerialNumber
 echo "New user $(dscl . -list /Users UniqueID | awk '{print $1}' | grep -w $UserName) has been created with unique ID $(dscl . -list /Users UniqueID | grep -w $UserName | awk '{print $2}')"
-
+echo "Enter admin login and password to activate FileVault"
 #enabling FileVault and add $UserName to list of users available to unlock encrypted disk
-sudo fdesetup enable 
-sudo fdesetup add -usertoadd "$UserName"
+sudo fdesetup enable -user "$CurrentUser" -usertoadd "$UserName"
+#sudo fdesetup enable 
+#printf "Enter admin login and password to add recently created user to group avaiable to unlock disk"
+#sudo fdesetup add -usertoadd "$UserName"
 printf "Users avaiable to unlock disk: %s\n$(sudo fdesetup list)'" | cut -d',' -f1 
 printf "Please reboot your computer\n"
