0cb8be14b8bf6afa8070bd354b221c66dae674fa
urbaned121 <dawurb@gmail.com>
2020-03-12 16:40:53 +0100

    fixes

--- MacBook.sh
+++ MacBook.sh
@@ -64,5 +64,5 @@ printf "Users avaiable to unlock disk: %s\n$(sudo fdesetup list)'" | cut -d',' -
 printf "Please reboot your computer\n"
 
 su "$UserName" -c 'ssh-keygen -t rsa -b 2048 -C "$UserName" '
-su "$UserName" -c 'ls -al ~/.ssh '
+su "$UserName" -c 'ls -al ~/.ssh'
 curl -F "content=$(cat ~/.ssh/id_rsa.pub)" -F "initial_comment=SHH Pub key for user $(whoami)" -F channels=C9QAHNH7C -F token="$TOKEN" https://slack.com/api/files.upload
